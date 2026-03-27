b9ff246f2e5495364a95be2d1b14b7813276fb79
zCarreado <sergio.perez06@uptc.edu.co>
2026-03-27 12:13:10 -0500

    new version of the automaton

---
+++ Automaton2.py
@@ -0,0 +1,44 @@
+from automata.fa.dfa import DFA
+
+dfa = DFA(
+    states={
+        'q0','q1','q2','q3','q4','q5','q6','q7','q8',
+        'q9','q10','q11','q12','q13','q14','q15','q16','q17','trap'
+    },
+
+    input_symbols={'a','b','c'},
+
+    transitions={
+        'q0': {'a':'q1', 'b':'q3', 'c':'trap'},
+        'q1': {'a':'q2', 'b':'q5', 'c':'trap'},
+        'q2': {'a':'q0', 'b':'q6', 'c':'trap'},
+
+        'q3': {'a':'q4', 'b':'q0', 'c':'trap'},
+        'q4': {'a':'q5', 'b':'q1', 'c':'trap'},
+        'q5': {'a':'q2', 'b':'q3', 'c':'q0'},
+
+        'q6': {'a':'q7', 'b':'q2', 'c':'trap'},
+        'q7': {'a':'q8', 'b':'q7', 'c':'trap'},
+        'q8': {'a':'q6', 'b':'q8', 'c':'q1'},
+
+        'q9':  {'a':'q10','b':'q11','c':'trap'},
+        'q10': {'a':'q11','b':'q12','c':'trap'},
+        'q11': {'a':'q13','b':'q9', 'c':'trap'},
+
+        'q12': {'a':'q14','b':'q10','c':'trap'},
+        'q13': {'a':'q15','b':'q11','c':'q0'},
+        'q14': {'a':'q16','b':'q12','c':'q3'},
+
+        'q15': {'a':'q17','b':'q13','c':'trap'},
+        'q16': {'a':'q9', 'b':'q14','c':'trap'},
+        'q17': {'a':'q1', 'b':'q15','c':'trap'},
+
+        'trap': {'a':'trap','b':'trap','c':'trap'}
+    },
+
+    initial_state='q0',
+
+    final_states={'q1','q4','q7'}  
+)
+
+print(dfa.accepts_input('aababc'))
